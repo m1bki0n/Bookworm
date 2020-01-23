9ffb70ccf04ab0dc530ec4a45bbc87337a102434
Makwan BK <42608332+m1bki0n@users.noreply.github.com>
2020-01-24 00:04:15 +0300

    Initial Commit

---
+++ Project 11/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project 11
+//
+//  Created by Makwan BK on 1/24/20.
+//  Copyright © 2020 Makwan BK. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
